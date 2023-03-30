dbf183366cace32526319d243fb2080d4e2a149d
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-03-30 12:39:24 +0700

    Membuat Login Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -14,4 +14,24 @@ class UserControllerTest extends TestCase
         $this->get('/login')
             ->assertSeeText('login');
     }
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            "user" => "daud",
+            "password" => "admin"
+        ])->assertRedirect("/")
+            ->assertSessionHas("user", "daud");
+    }
+    public function testValidationError(){
+        $this->post('login', [])
+            ->assertSeeText('User or password is required');
+    }
+    public function testloginFailed()
+    {
+        $this->post('/login', [
+            'user' => 'daud',
+            'password' => 'salahpassword'
+        ])->assertSeeText('User or password wrong');
+    }
+
 }
